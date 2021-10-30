a9510eb717ee86e3ea9d26a53b1f3725fd9f8011
FlowingSkiff <boats303@gmail.com>
2021-10-30 16:21:35 -0500

    Stores all items as a member

--- UI/primativeDesign.hpp
+++ UI/primativeDesign.hpp
@@ -1,13 +1,26 @@
 #pragma once
 #include "baseProject.h"
+#include <vector>
+#include <memory>
+#include "SQObject.hpp"
+#include "StringTools.hpp"
+namespace Creator::Entity{struct SQObject; }
+
+static wxString ProcessHTMLDescription(const std::string& str)
+{
+    return "<!DOCTYPE html><html><body>" + ReplaceSpecialInString(str) + "</body></html>";
+}
 
 class TmpDisplay: public MyFrame1
 {
     protected:
-        virtual void ReloadHtmlDescription(wxCommandEvent& event)
+        virtual void ReloadHtmlDescription(wxCommandEvent& event) wxOVERRIDE
         {
+            m_htmlWin1->SetPage(ProcessHTMLDescription(m_objects.at(m_choice1->GetStringSelection().ToStdString())->description));
             event.Skip();
         }
     public:
         TmpDisplay(): MyFrame1(nullptr) { }
+        std::unordered_map<std::string, std::shared_ptr<Creator::Entity::SQObject>> m_objects;
 };
+
